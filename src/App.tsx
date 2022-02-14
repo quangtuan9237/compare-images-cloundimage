fd605b8d89cba0db7030c21ea8d6cc5fe71b6597
Tuan Duong <quangtuan9237@gmail.com>
2022-02-14 20:44:51 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import GlobalStyles from './components/GlobalStyles'
+
+function App() {
+
+  return (
+    <div>
+      <h1>hello world</h1>
+      <GlobalStyles />
+    </div>
+  )
+}
+
+export default App
